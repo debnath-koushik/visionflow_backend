c467e263d408d36da56213fdbb2c09e9c8332f53
Koushik Debnath <111985459+debnath-koushik@users.noreply.github.com>
2025-11-12 11:52:25 +0530

    change CORS settings

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["http://localhost:5173", "http://localhost:3000"],  # Add your frontend origins
-    # allow_credentials=True,
-    allow_origins=["*"],  # Allow all origins
-    allow_credentials=False,
+    allow_origins=["http://localhost:5173", "http://localhost:3000"],  # Add your frontend origins
+    allow_credentials=True,
+    # allow_origins=["*"],  # Allow all origins
+    # allow_credentials=False,
     allow_methods=["*"],  # Allow all methods
     allow_headers=["*"],  # Allow all headers
     expose_headers=["X-Matched-Files", "X-Extra-Labels-Removed", "X-Extra-Images-Removed", "X-Files-Added", "X-Train-Count", "X-Valid-Count", "X-Test-Count", "X-Total-Count", "X-Original-Duration", "X-Trimmed-Duration", "X-Total-Frames", "X-Extracted-Frames", "X-Frame-Gap", "X-FPS"],  # Expose custom headers
@@ -568,3 +568,4 @@ async def frame_splitting(file: UploadFile = File(...), frame_gap: int = Form(..
             cap.release()
 
         raise HTTPException(status_code=500, detail=str(e))
+
